19ab25968b6048c1bb2500fbf48ef2c58f1f4354
Jeremy Muhlich <jmuhlich@bitflood.org>
2012-04-05 23:48:12 -0400

    got clean scrolling working

--- map.py
+++ map.py
@@ -8,34 +8,23 @@ def exit_game():
     pygame.quit()
     sys.exit()
 
+pygame.init()
+
+# load tiles
 tile_base = os.path.join('data', 'base')
 tile_names = ('grass', 'dirt', 'sand', 'snow', 'water')
-tiles = {}
+tiles = []
 for name in tile_names:
     path = os.path.join(tile_base, name + '.png')
-    tiles[name] = pygame.image.load(path)
-print tiles
+    tiles.append(pygame.image.load(path))
 
+# create random world map
 np.random.seed(0)
 world_map = np.random.randint(len(tile_names) - 1, size=(100,100))
-#world_map[10:20, 3:7] = len(tile_names) - 1
+# add a lake
+world_map[10:20, 3:7] = len(tile_names) - 1
 
-world_map[0,:] = 0
-world_map[1,:] = 1
-world_map[2,:] = 2
-world_map[3,:] = 3
-world_map[4,:] = 0
-world_map[5,:] = 1
-world_map[6,:] = 2
-world_map[7,:] = 3
-world_map[:,0] = 0
-world_map[:,1] = 1
-world_map[:,2] = 2
-world_map[:,3] = 3
-world_map[:,4] = 4
-
-pygame.init()
-screen = pygame.display.set_mode((640, 480), 0, 32)
+screen = pygame.display.set_mode((640, 480), 0, 24)
 print pygame.display.Info()
 
 last_fps_tick = 0
@@ -43,22 +32,20 @@ clock = pygame.time.Clock()
 
 keys = {}
 camera = [32,16]
-camera = [0,0]
 
 while True:
-    for y in range(-1,33):
+    # render the map
+    for y in range(-1,31):
         for x in range(-1,11):
-            # FIXME this is still wonky when camera.y&16==16
             wx = x + (camera[0] / 64)
             wy = y + (camera[1] / 16)
-            #if x==0 and y==0:
-            #    print wx, wy, camera[0], camera[1]
-            #    sys.stdout.flush()
-            tile = tiles[tile_names[world_map[wy][wx]]]
-            sx = x * 64 + (y & 1) * 32 - (camera[0] % 64)
+            tile = tiles[world_map[wy,wx]]
+            # FIXME: this sx/xy math is out of control
+            sx = x * 64 + (y & 1) * 32 - (camera[0] % 64) - (camera[1]/16&1)*32 + (camera[1]/16&wy&1)*64
             sy = (y >> 1) * 32 + (y & 1) * 16 - (camera[1] % 16)
             screen.blit(tile, (sx, sy))
 
+    # handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit_game()
@@ -66,20 +53,24 @@ while True:
             keys[event.key] = True
         if event.type == pygame.KEYUP:
             keys[event.key] = False
+
+    # handle key states
     if keys.get(pygame.K_ESCAPE):
         exit_game()
     if keys.get(pygame.K_LEFT):
-        camera[0] -= 1
+        camera[0] -= 3
     if keys.get(pygame.K_RIGHT):
-        camera[0] += 1
+        camera[0] += 3
     if keys.get(pygame.K_UP):
-        camera[1] -= 1
+        camera[1] -= 3
     if keys.get(pygame.K_DOWN):
-        camera[1] += 1
+        camera[1] += 3
 
-    #camera[0] = max(camera[0], 32)
-    #camera[1] = max(camera[1], 16)
-
+    # keep camera inside the map
+    camera[0] = max(camera[0], 32)
+    camera[1] = max(camera[1], 16)
+    camera[0] = min(camera[0], (world_map.shape[1]-1)*64-screen.get_width())
+    camera[1] = min(camera[1], (world_map.shape[0]-1)*16-screen.get_height())
 
     clock.tick(60)
     pygame.display.flip()
