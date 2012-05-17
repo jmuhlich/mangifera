86edaf3353b9cf0a6d31197a3412f138e6812909
Jeremy Muhlich <jmuhlich@bitflood.org>
2012-05-16 23:36:56 -0400

    switched to a linear camera projection

--- map.py
+++ map.py
@@ -1,12 +1,21 @@
 import pygame
-import numpy as np
+import numpy
 import os
 import sys
 import random
 
 pygame.init()
-screen = pygame.display.set_mode((640, 480))
+screen_size = (640, 480)
+screen = pygame.display.set_mode(screen_size)
 print pygame.display.Info()
+
+# screen middle x and y
+smx = screen_size[0] / 2
+smy = screen_size[1] / 2
+
+grid_w, grid_h = 64, 32
+grid_w_half = grid_w / 2
+grid_h_half = grid_h / 2
 
 # load tiles
 tile_base = 'data'
@@ -21,14 +30,23 @@ for group, names in tile_names:
         tiles.append(pygame.image.load(path).convert_alpha())
 
 # create random world map
-np.random.seed(0)
-world_map = np.random.randint(4, size=(100,100))
+numpy.random.seed(0)
+world_map = numpy.random.randint(4, size=(100,100))
 # add a lake
 world_map[10:20, 2:6] = 4
 # add walls
 world_map[15, 8] = 5
-world_map[14, 9] = 6
-world_map[14, 8] = 7
+world_map[16, 8] = 6
+world_map[15, 7] = 7
+
+# index of lowest tile which can't be seen through
+min_opaque = 5
+
+# camera movement vectors
+camera_right = numpy.array([1, 1], float) / 16
+camera_down = numpy.array([1, -1], float) / 16
+camera_speed_normal = 1
+camera_speed_fast = 10
 
 clock = pygame.time.Clock()
 
@@ -36,23 +54,24 @@ clock = pygame.time.Clock()
 def mainloop():
 
     keys = {}
-    camera = [32,16]
+    camera = numpy.array([10,10], dtype=float)
+    camera_speed = camera_speed_normal
     last_fps_tick = 0
 
     while True:
 
+        screen.fill(0)
+
         # render the map
-        for y in range(-1,31):
-            for x in range(-1,11):
-                wx = x + (camera[0] / 64)
-                wy = y + (camera[1] / 16)
+        for wy in range(world_map.shape[1] - 1, -1, -1):
+            for wx in range(world_map.shape[0]):
                 tile = tiles[world_map[wy,wx]]
-                # FIXME: this sx/xy math is out of control
-                sx = x * 64 + (y & 1) * 32 - (camera[0] % 64) - (camera[1]/16&1)*32 + (camera[1]/16&wy&1)*64
-                sy = (y >> 1) * 32 + (y & 1) * 16 - (camera[1] % 16)
-                sx -= tile.get_width() / 2 - 32
-                sy -= tile.get_height() - 31
-                screen.blit(tile, (sx, sy))
+                sx, sy = world_to_screen(camera, wx, wy)
+                if -grid_w < sx < screen_size[0] \
+                        and -grid_h < sy < screen_size[1]:
+                    sx -= tile.get_width() / 2 - 32
+                    sy -= tile.get_height() - 31
+                    screen.blit(tile, (sx, sy))
 
         # handle events
         for event in pygame.event.get():
@@ -66,20 +85,18 @@ def mainloop():
         # handle key states
         if keys.get(pygame.K_ESCAPE):
             exit_game()
+        if keys.get(pygame.K_LSHIFT) or keys.get(pygame.K_RSHIFT):
+            camera_speed = camera_speed_fast
+        else:
+            camera_speed = camera_speed_normal
         if keys.get(pygame.K_LEFT):
-            camera[0] -= 3
+            camera -= camera_right * camera_speed
         if keys.get(pygame.K_RIGHT):
-            camera[0] += 3
+            camera += camera_right * camera_speed
         if keys.get(pygame.K_UP):
-            camera[1] -= 3
+            camera -= camera_down * camera_speed
         if keys.get(pygame.K_DOWN):
-            camera[1] += 3
-
-        # keep camera inside the map
-        camera[0] = max(camera[0], 32)
-        camera[1] = max(camera[1], 16)
-        camera[0] = min(camera[0], (world_map.shape[1]-1)*64-screen.get_width())
-        camera[1] = min(camera[1], (world_map.shape[0]-1)*16-screen.get_height())
+            camera += camera_down * camera_speed
 
         clock.tick(60)
         pygame.display.flip()
@@ -87,6 +104,16 @@ def mainloop():
         if cur_tick >= last_fps_tick + 1000:
             last_fps_tick = cur_tick
             print 'FPS:', clock.get_fps()
+
+
+def world_to_screen(camera, wx, wy):
+    # make wx,wy relative to camera look-at point
+    wx -= float(camera[0])
+    wy -= float(camera[1])
+    # transform into screen space
+    sx = smx + (wx + wy) * grid_w_half
+    sy = smy + (wx - wy) * grid_h_half
+    return sx, sy
 
 
 def exit_game():
